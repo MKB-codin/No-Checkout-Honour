aa2da7e01c22ac1c620dffd0b3a31231c0ebd835
KOMAIL BAIG <M.K.BAIG-2021@hull.ac.uk>
2025-01-22 16:42:58 +0000

    Created the Login Page.
    Current functionality :
    Allows the user to enter email and password.
    Ensures email is in a valid email form (eg.  x@y.com)

---
+++ SelfCheckoutApp/Pages/LoginPage.xaml.cs
@@ -0,0 +1,69 @@
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+
+namespace SelfCheckoutApp.Pages
+{
+    public partial class LoginPage : ContentPage
+    {
+        private readonly HttpClient _httpClient;
+
+        public LoginPage()
+        {
+            InitializeComponent();
+
+
+            _httpClient = new HttpClient { BaseAddress = new Uri("https://localhost:5001") };
+
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            ErrorMessage.IsVisible = false;
+            string email = EmailEntry.Text;
+            string password = PasswordEntry.Text;
+
+            if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            {
+                ErrorMessage.Text = "Email and Password are required.";
+                ErrorMessage.IsVisible = true;
+                return;
+            }
+
+            if (!IsValidEmail(email))
+            {
+                ErrorMessage.Text = "Please enter a valid email address.";
+                ErrorMessage.IsVisible = true;
+                return;
+            }
+
+            var loginRequest = new { email, password };
+
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("/api/Users/Login", loginRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    await DisplayAlert("Success", "Login successful!", "OK");
+                    // remmeber to add the next page navi here
+                }
+                else
+                {
+                    ErrorMessage.Text = "Invalid email or password.";
+                    ErrorMessage.IsVisible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorMessage.Text = $"Error: {ex.Message}";
+                ErrorMessage.IsVisible = true;
+            }
+        }
+        private bool IsValidEmail(string email)
+        {
+            string emialPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, emialPattern);
+        }
+    }
+
+}
--- SelfCheckoutApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SelfCheckoutApp.AssemblyInfo.cs
+++ SelfCheckoutApp/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/SelfCheckoutApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SelfCheckoutApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c28b7e5adb901522d36f57b702e039ef7636d6de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0cc69b18a31a8f1c23b949b68d01159f21f289a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SelfCheckoutApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SelfCheckoutApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
